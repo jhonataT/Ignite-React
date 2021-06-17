88158a2d53ac14ce5aed13efcb6dce99fadc848d
Jhn <jhonata.tenorio2526@gmail.com>
2021-06-17 17:41:26 -0300

    Configurando Estrutura do ReactJS

--- Chapter_I/01-github-explorer/src/index.jsx
+++ Chapter_I/01-github-explorer/src/index.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
+import { render } from 'react-dom';
 import { App } from './App';
 
-function Wrapper() {
-    return <App />
-}
+render(<App />, document.getElementById('root'));
